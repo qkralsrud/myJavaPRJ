7c69b1e8f4a5c3cd90ae6e799dd778edb72a0fce
qkralsrud <qkralsrud0014@gmail.com>
2023-03-15 11:48:50 +0900

    ex03-05

---
+++ src/main/java/Ex03_07.java
@@ -0,0 +1,9 @@
+public class Ex03_07 {
+    public static void main(String[] args) {
+        float a = 0.1235565801234234567879223456735f;
+        double b = 0.1234324575870152345667879234374;
+
+        System.out.printf("%30.25f \n", a);
+        System.out.printf("%30.25f \n", b);
+    }
+}
