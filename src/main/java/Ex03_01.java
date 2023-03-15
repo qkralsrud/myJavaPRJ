437540a1372e83ba186ddcc202f2c08bc3af6727
qkralsrud <qkralsrud0014@gmail.com>
2023-03-15 10:33:53 +0900

    Ex03

--- src/main/java/Ex03_01.java
+++ src/main/java/Ex03_01.java
@@ -1,8 +1,12 @@
 public class Ex03_01 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.printf("100+100");
-        System.out.printf("\n");
         System.out.printf("%d", 100 + 100);
-        System.out.printf("\n");
     }
+//    public static void main(String[] args){
+//        System.out.printf("100+100");
+//        System.out.printf("\n");
+//        System.out.printf("%d", 100 + 100);
+//        System.out.printf("\n");
+//    }
 }
