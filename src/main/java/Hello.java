4a69cec8d27618ff6dc7be3950727cf4edb9a319
qkralsrud <qkralsrud0014@gmail.com>
2023-03-15 09:28:12 +0900

    first file

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("Sysetem out println");
+        System.out.println("public static void main");
+    }
+}
