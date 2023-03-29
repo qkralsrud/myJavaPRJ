38d750fe0b152c9368e6993dca6bfbb3733bbb14
qkralsrud <qkralsrud0014@gmail.com>
2023-03-29 11:13:52 +0900

    for

---
+++ src/main/java/Ex06_01.java
@@ -0,0 +1,11 @@
+public class Ex06_01 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 5; i++)
+        {
+            System.out.printf("안녕하세요.\n");
+        }
+    }
+}
+
