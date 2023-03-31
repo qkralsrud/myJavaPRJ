3827b961fb163a9926f2e663f38cd6492a190f2e
qkralsrud <qkralsrud0014@gmail.com>
2023-03-31 21:43:05 +0900

    23.03.31 빼기

---
+++ src/main/java/Hw04_06.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hw04_06 {
+    public static void main(String[] args) {
+        int y;
+
+        Scanner s= new Scanner(System.in);
+        y=s.nextInt();
+
+        System.out.printf("%d",y-543);
+
+
+
+    }
+}
