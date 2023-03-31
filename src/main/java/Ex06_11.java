52c19f551c94e1bdf6b34c76e6ee549e2aa25a8f
qkralsrud <qkralsrud0014@gmail.com>
2023-03-31 20:36:14 +0900

    23.03.31

---
+++ src/main/java/Ex06_11.java
@@ -0,0 +1,14 @@
+public class Ex06_11 {
+    public static void main(String[] args) {
+        int i, k;
+
+        for(i = 0; i < 5; i++){
+                for(k = 0;k <=i; k++){
+                    System.out.printf("*");
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+
