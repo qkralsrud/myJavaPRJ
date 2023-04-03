40c697502436229a22d5a2e42496c2722dd1ba15
qkralsrud <qkralsrud0014@gmail.com>
2023-04-03 16:05:02 +0900

    23.04.03 문자열 출력

---
+++ src/main/java/Hw05_01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Hw05_01 {
+    public static void main(String[] args){
+        Scanner s =new Scanner(System.in);
+
+        String a; //String 문자열 입력
+        a =s.nextLine();
+
+        System.out.printf("%s??!",a); //%s 문자열 출력
+                                        //%c 철자 출력
+
+
+
+
+    }
+
+}
