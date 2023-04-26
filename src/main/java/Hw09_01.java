b3e0f20dcd769c75769b4df2798ddb34d1b7df15
qkralsrud <qkralsrud0014@gmail.com>
2023-04-26 09:46:13 +0900

    23.04.

---
+++ src/main/java/Hw09_01.java
@@ -0,0 +1,15 @@
+public class Hw09_01 {
+    public static void main(String[] args) {
+    int i,k;
+
+    for (i=5;i>0;i--){
+        for (k=0; k<i;k++) {
+//            System.out.printf(" ");
+//            if ()
+            System.out.printf("%d", i);
+        }
+        System.out.printf("\n");
+    }
+
+    }
+}
